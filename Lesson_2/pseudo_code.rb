91ab60389e99eea00f6a1e0f8fe52b033bbe047d
Michael Roker <mroker@gmail.com>
2016-06-07 16:19:04 -0400

    Added two files for lesson 2

---
+++ Lesson_2/pseudo_code.rb
@@ -0,0 +1,23 @@
+#a method that returns the sum of two integers
+
+Create function
+  - take arguments and add them
+  - end function
+
+
+# a method that takes an array of strings, and returns a string 
+#that is all those strings concatenated together
+
+Create method
+  - create a variable that is a string
+  - iterate over the array of strings
+  - append each string to the variable string
+
+
+
+#a method that takes an array of integers, 
+#and returns a new array with every other element
+
+Create method
+  - run for loop that increases + 2
+  - create an array for every loop
