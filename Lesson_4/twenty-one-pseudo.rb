7bf99bf18e23d406ecea7623260fb68d52faf53a
Michael Roker <mroker@gmail.com>
2016-07-12 12:34:45 -0400

    add beginning of 21

---
+++ Lesson_4/twenty-one-pseudo.rb
@@ -0,0 +1,10 @@
+1. Initialize deck
+  - create a method that makes a range of a deck  and assigns is values from 1 - 10?
+2. Deal cards to player and dealer
+3. Player turn: hit or stay
+  - repeat until bust or "stay"
+4. If player bust, dealer wins.
+5. Dealer turn: hit or stay
+  - repeat until total >= 17
+6. If dealer bust, player wins.
+7. Compare cards and declare winner.
---
+++ Lesson_4/twenty-one.rb
@@ -0,0 +1,5 @@
+deck = []
+
+(1..10).push(deck)
+
+p deck
