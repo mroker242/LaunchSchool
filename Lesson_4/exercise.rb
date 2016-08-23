b5bba9b087566e0ff5b85039772e965679f72acd
Michael Roker <mroker@gmail.com>
2016-08-23 10:48:11 -0400

    Assessment changes

--- Lesson_4/exercise.rb
+++ Lesson_4/exercise.rb
@@ -3,25 +3,17 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
-def sequence(num)
-  (1..num).inject([]) do |memo, word|
-
-    memo << word
-
-  end
-
-
-end
+array1 = %w(Moe Larry Curly Chemp Harpo Chico Groucho Zeppo)
+array2 = []
+array1.each { |value| array2 << value }
+array1.each { |value| value.upcase! if value.start_with?('C') }
+puts array2
 
 
+# array2 = [Moe,Larry,Curly,Chemp,Harpo,Chico,Groucho,Zeppo]
 
-p sequence(5)
-        
-        
-        
-        
-        
-        
+# array1 = [Moe,Larry,CURLY,CHEMP,Harpo,CHICO,Groucho,Zeppo]
+
 
 
 
